3fda9bbd1abb7a543f814ca185314f8705bee90b
Giorgio Matessi <giorgio.matessi@gmail.com>
2024-03-17 19:02:01 -0600

    cors

--- src/app/shared/api.ts
+++ src/app/shared/api.ts
@@ -28,8 +28,6 @@ export async function postItem(item: Item) {
     body: generateDataString(item),
     headers: {
       'Content-Type': 'application/json',
-      'Access-Control-Allow-Origin': '*',
-      'Access-Control-Allow-Credentials': 'true',
     },
   });
 }
